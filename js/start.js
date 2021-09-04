02097ab6ce4c75e4baf08f1714a066809b648bca
bratik026 <boyenerji2014@yandex.ru>
2021-09-04 18:23:54 +0300

    04/09/21 18:23

--- js/start.js
+++ js/start.js
@@ -1,66 +1,150 @@
 "use strict";
 
 
+
+let testText = '';
 let lock = {
     name: "&#128274;",
-    age: randomInteger(1, 12)
+    age: randomInteger(1, 30)
 };
 
 let fire = {
     name: "&#128293;",
-    age: randomInteger(1, 12)
+    age: randomInteger(1, 30)
 };
 
 let lypa = {
     name: "&#128269;",
-    age: randomInteger(10, 20)
+    age: randomInteger(1, 30)
 };
 
-if (lock.age == fire.age) {
-    while (lock.age == fire.age) {
-        lock.age = randomInteger(1, 30);
-    }
-}
 
-if (lock.age == lypa.age) {
-    while (lock.age == lypa.age) {
+if (lock.age == fire.age || lock.age == lypa.age || fire.age == lypa.age){
+    while (lock.age == fire.age || lock.age == lypa.age || fire.age == lypa.age){
         lock.age = randomInteger(1, 30);
-    }
-}
-
-if (fire.age == lypa.age) {
-    while (fire.age == lypa.age) {
         fire.age = randomInteger(1, 30);
     }
 }
 
 
-let arrFive = [lock, fire, lypa];
 
+
+
+let knopkiOne = {
+    numberOne: `<input type="submit" id="number" value="${lypa.age}" onclick="check(this)"></input>`,
+    age: randomInteger(1, 30)
+};
+
+let knopkiTwo = {
+    numberOne: `<input type="submit" id="number" value="${randomInteger(1, 30)}" onclick="check(this)"></input>`,
+    age: randomInteger(1, 30)
+};
+
+let knopkiThree = {
+    numberOne: `<input type="submit" id="number" value="${randomInteger(1, 30)}" onclick="check(this)"></input>`,
+    age: randomInteger(1, 30)
+};
+
+let knopkiFour = {
+    numberOne: `<input type="submit" id="number" value="${fire.age}" onclick="check(this)"></input>`,
+    age: randomInteger(1, 30)
+};
+
+let knopkiFive = {
+    numberOne: `<input type="submit" id="number" value="${randomInteger(1, 30)}" onclick="check(this)"></input>`,
+    age: randomInteger(1, 30)
+};
+
+let knopkiSix = {
+    numberOne: `<input type="submit" id="number" value="${lock.age}" onclick="check(this)"></input>`,
+    age: randomInteger(1, 30)
+};
+
+let arrKnopki = [knopkiOne, knopkiTwo, knopkiThree, knopkiFour, knopkiFive, knopkiSix];
+
+sortByAge(arrKnopki);
+
+
+const buttonReload = '<input type="submit" id="reload" value="Начать заново" onclick="update(this)"></input>';
+
+
+
+
+
+// if (lock.age == fire.age || lock.age == lypa.age || fire.age == lypa.age){
+//     while (lock.age == fire.age || lock.age == lypa.age || fire.age == lypa.age){
+//         lock.age = randomInteger(1, 30);
+//         fire.age = randomInteger(1, 30);
+//     }
+// }
+
+// if (lock.age == fire.age) {
+//     while (lock.age == fire.age) {
+//         lock.age = randomInteger(1, 30);
+//     }
+// }
+
+// if (lock.age == lypa.age) {
+//     while (lock.age == lypa.age) {
+//         lock.age = randomInteger(1, 30);
+//     }
+// }
+
+// if (fire.age == lypa.age) {
+//     while (fire.age == lypa.age) {
+//         fire.age = randomInteger(1, 30);
+//     }
+// }
+
+
+let arrFive = [lock, fire, lypa];
 sortByAge(arrFive);
 
 
-document.getElementById('test').innerHTML += arrFive[0].name + ' = ' + arrFive[0].age;
-setTimeout(() => document.getElementById("test").innerHTML = "", 2000);
-
-setTimeout(() => document.getElementById('test').innerHTML += arrFive[1].name + ' = ' + arrFive[1].age, 3000);
-setTimeout(() => document.getElementById("test").innerHTML = "", 5000);
-
-setTimeout(() => document.getElementById('test').innerHTML += arrFive[2].name + ' = ' + arrFive[2].age, 7000);
-setTimeout(() => document.getElementById("test").innerHTML = "", 9000);
 
 
-setTimeout(() => document.getElementById('test').innerHTML += `Сколько было в ${arrFive[2].name}`, 11000);
+document.getElementById('test').innerHTML += '<div class="qwe">' + arrFive[0].name + ' = ' + arrFive[0].age + '</div>';
+setTimeout(() => document.getElementById("test").innerHTML = "", 1500);
 
-setTimeout(() => document.getElementById('testPole').innerHTML += '<input type="text"></input>', 11500);
-setTimeout(() => document.getElementById('testPole').innerHTML += '<input type="submit" onclick="check(this)"></input>', 11500);
+setTimeout(() => document.getElementById('test').innerHTML += '<div class="qwe">' + arrFive[1].name + ' = ' + arrFive[1].age + '</div>', 3000);
+setTimeout(() => document.getElementById("test").innerHTML = "", 4500);
+
+setTimeout(() => document.getElementById('test').innerHTML += '<div class="qwe">' + arrFive[2].name + ' = ' + arrFive[2].age + '</div>', 6000);
+setTimeout(() => document.getElementById("test").innerHTML = "", 7500);
+
+
+setTimeout(() => document.getElementById('test').innerHTML += `Сколько было в ${randomIntegerTwo(0,2)}`, 7600);
+
+
+
+setTimeout(function() {
+    let testText = document.getElementById('test').innerHTML;
+    if (testText == 'Сколько было в 🔍' || testText == 'Сколько было в 🔥' || testText == 'Сколько было в 🔒') {
+        for (let index = 0; index < arrFive.length; index++) {
+            console.log(arrFive[index].name);
+            if (arrFive[index].name == '&#128269;') {
+                for (let indexTwo = 0; indexTwo < arrKnopki.length; indexTwo++) {
+                    document.getElementById('testPole').innerHTML += arrKnopki[indexTwo].numberOne;
+                }        
+            }
+        }
+        for (let indexTwo = 0; indexTwo < arrKnopki.length; indexTwo++) {
+            document.getElementById('testPole').innerHTML += arrKnopki[indexTwo].numberOne;
+        }   
+    }
+}, 7700);
+
 
 
 let counter = 0;
 let complete = 0;
+let arrErrors = [];
 function check(elem) {
     complete++;
-    let input = document.querySelector('input').value;
+    // let input = document.querySelectorAll('input');
+    let input = elem.value;
+
+    console.log(elem);
 
     let text = document.getElementById('test').innerHTML;
     console.log(text);
@@ -70,50 +154,41 @@ function check(elem) {
         for (let index = 0; index < arrFive.length; index++) {
             if (arrFive[index].name == '&#128269;') {
                 if (arrFive[index].age == input) {
-                    console.log('Значение ЛУПЫ совпадает с тем что я ввел');
                     counter++;
-                } else {
-                    console.log('Значение в ЛУПЕ не совпадает');
                 }
-            document.getElementById("test").innerHTML = "";
-            arrFive.splice(index, 1);
-            } 
+                arrFive.splice(index, 1);
+            }
         }
     } else if (text == 'Сколько было в 🔥') {
         for (let index = 0; index < arrFive.length; index++) {
             if (arrFive[index].name == '&#128293;') {
                 if (arrFive[index].age == input) {
-                    console.log('Значение ОГНЯ совпадает с тем что я ввел');
                     counter++;
-                } else {
-                    console.log('Значение в ОГНЯ не совпадает');
                 }
-            document.getElementById("test").innerHTML = "";
-            arrFive.splice(index, 1);
-            } 
+                arrFive.splice(index, 1);
+            }
         }
-
     } else if (text == 'Сколько было в 🔒') {
         for (let index = 0; index < arrFive.length; index++) {
             if (arrFive[index].name == '&#128274;') {
                 if (arrFive[index].age == input) {
-                    console.log('Значение ЗАМКА совпадает с тем что я ввел');
                     counter++;
-                } else {
-                    console.log('Значение в ЗАМКА не совпадает');
                 }
-            document.getElementById("test").innerHTML = "";
-            arrFive.splice(index, 1);
-            } 
+                arrFive.splice(index, 1);
+            }
         }
     }
+    elem.remove();
+    document.getElementById("test").innerHTML = "";
 
     if (complete == 3) {
         if (counter == 3) {
             document.getElementById('test').innerHTML += 'Без ошибок';
         } else {
-            document.getElementById('test').innerHTML += 'Были ошибки';
-        } 
+            document.getElementById('test').innerHTML += 'С ошибками';
+        }
+        setTimeout(() => document.getElementById("testPole").innerHTML = "", 1000);
+        setTimeout(() => document.getElementById('testPole').innerHTML += buttonReload, 2000);
     }
 
     if (arrFive.length != 0) {
@@ -121,11 +196,11 @@ function check(elem) {
             document.getElementById('test').innerHTML += 'Сколько было в ' + arrFive[one].name;
         }
     }
-
-    document.querySelector('input').value = '';
-    document.querySelector('input').focus(); 
 }
 
+function update(elem) {
+    document.location.reload();
+}
 
 
 function sortByAge(arrFive) {
@@ -136,3 +211,10 @@ function randomInteger(min, max) {
     let rand = min - 0.5 + Math.random() * (max - min + 1);
     return Math.round(rand);
 }
+
+function randomIntegerTwo(min, max) {
+    let randTwo = Math.round(min - 0.5 + Math.random() * (max - min + 1));
+    return arrFive[randTwo].name;
+}
+
+
